37df5d027eafc54f2da2dd8fa7f76395eb69ea91
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-11-22 11:18:26 +0100

    edit of the number of cluster

--- recommendation.py
+++ recommendation.py
@@ -17,12 +17,12 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 sp = Spotify(client_credentials_manager=client_credentials_manager)
 
 # Charger le jeu de données (supposons qu'il soit au format CSV)
-df = pd.read_csv("data_by_genres.csv")
+df = pd.read_csv("data/data_by_genres.csv")
 data=df.drop('genres', axis=1)
 feature=['mode','acousticness','danceability','duration_ms','energy','instrumentalness','liveness','loudness','speechiness','mode','tempo','valence','popularity','key']
 scaler = StandardScaler()
 dataScaler = scaler.fit_transform(data[feature])
-model = KMeans(n_clusters=5)
+model = KMeans(n_clusters=10)
 df['label'] = model.fit_predict(dataScaler)
 
 # Fonction pour chercher les genres
